352f60a2ab223b5a7cc066705fafcaf309a48076
Ramadhani Musthofa <ramadhani.musthofa@gdn-commerce.com>
2023-03-17 16:21:32 +0700

    multiple caret support

--- signed.sh
+++ signed.sh
@@ -1,6 +1,6 @@
 java -jar publish/marketplace-zip-signer-cli.jar sign\
-  -in "build/distributions/unix-timestamp-4.1.3.zip"\
-  -out "build/distributions/unix-timestamp-4.1.3-signed.zip"\
+  -in "build/distributions/unix-timestamp-4.1.4.zip"\
+  -out "build/distributions/unix-timestamp-4.1.4-signed.zip"\
   -cert-file "publish/chain.crt"\
   -key-file "publish/private.pem"\
-  -key-pass {{password}}
+  -key-pass Istrikusukakucing110

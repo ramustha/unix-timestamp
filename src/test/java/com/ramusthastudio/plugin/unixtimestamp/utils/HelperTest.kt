33de4e57d06d78e455d775f8fb0765d7386f63fa
ramustha <ramusthanest@gmail.com>
2023-11-22 16:52:40 +0700

    logic improvement for suffix l or L

--- src/test/java/com/ramusthastudio/plugin/unixtimestamp/utils/HelperTest.kt
+++ src/test/java/com/ramusthastudio/plugin/unixtimestamp/utils/HelperTest.kt
@@ -62,6 +62,28 @@ class HelperTest : StringSpec({
         result[1] shouldBe timestamp2
     }
 
+    "findUnixTimestamp should return the correct unique UNIX timestamps contains suffix l or L" {
+        // Create a large string with sentences mixed with unix timestamps
+        val timestamp1 = 1688849864454L
+        val timestamp2 = "1688849864455l"
+
+        val largeString = buildString {
+            repeat(5000) {
+                append("This is a sample sentence timestamp: $timestamp1. ")
+                append("This is another sample sentence timestamp: $timestamp2. ")
+            }
+        }
+
+        val result = Helper.findUnixTimestamp(largeString)
+
+        // Assert the size of the result
+        result shouldHaveSize 2
+
+        // Assert that the correct timestamps were found
+        result[0] shouldBe timestamp1.toString().dropLast(1)
+        result[1] shouldBe timestamp2.dropLast(1)
+    }
+
     "findTextRanges should return correct ranges when searching for a timestamp" {
         val timeStamp = "1691475292429"
         val text = "Hey there! Timestamp is here: 1691475292429. That's it."

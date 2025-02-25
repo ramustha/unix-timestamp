46867091544d15ebcb6442d6ba3086259fe957a5
ramustha <ramusthanest@gmail.com>
2025-02-25 18:03:15 +0700

    logic improvement

--- src/main/kotlin/com/ramusthastudio/plugin/unixtimestamp/hints/BaseInlayHintsCollector.kt
+++ src/main/kotlin/com/ramusthastudio/plugin/unixtimestamp/hints/BaseInlayHintsCollector.kt
@@ -13,11 +13,18 @@ class BaseInlayHintsCollector<T : PsiElement?>(
     val editor: Editor,
     private val psiElement: Class<T>,
     private val settingsState: AppSettingsState = AppSettingsState.instance,
-    private val uniqueIndices: MutableSet<Int> = mutableSetOf()
+    private val uniqueIndices: MutableSet<Int> = FixedSizeSet(1000) // Limit to 1000 entries
 ) : SharedBypassCollector {
     override fun collectFromElement(element: PsiElement, sink: InlayTreeSink) {
         if (psiElement.isInstance(element)) {
             createInlayHintsElement(uniqueIndices, element, sink, settingsState)
         }
+    }
+}
+
+class FixedSizeSet<T>(private val maxSize: Int) : LinkedHashSet<T>() {
+    override fun add(element: T): Boolean {
+        if (size >= maxSize) remove(first()) // Remove the oldest element
+        return super.add(element)
     }
 }
